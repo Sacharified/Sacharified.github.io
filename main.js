b5d9275077735406e0ff8982a838421ebc5efb7c
sacha <sacha.guddoy@gmail.com>
2015-10-24 17:18:34 +0100

    added re-select button and fixed selection bug

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-
 var map;
+var radius = 500;
+var selected;
+
 function mapInit() {
+    var London = new google.maps.LatLng( 51.5287718, -0.2416814);
     map = new google.maps.Map(document.getElementById('map'), {
-        center: new google.maps.LatLng(0,0),
+        center: London,
         zoom: 15
     });
     getCurrentPos();
@@ -11,17 +14,22 @@ function getCurrentPos() {
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             var current_pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-            if(current_pos != undefined) findLocal(current_pos);
+            if(current_pos !== undefined)
+                findLocal(current_pos);
         });
     }
 }
 
-function findLocal(current_pos) {
+function reselect() {
+    console.log('click');
+    getCurrentPos();
+}
 
-    map.setCenter(current_pos);
+function findLocal(target_pos) {
+    map.setCenter(target_pos);
     var request = {
-        location: current_pos,
-        radius: '500',
+        location: target_pos,
+        radius: radius,
         types: ['cafe', 'food', 'meal_takeaway', 'meal_delivery']
     };
 
@@ -34,26 +42,30 @@ function findLocal(current_pos) {
             randomiseChoices(results);
     }
 }
+
+
 function randomiseChoices(results) {
-   var rng = Math.floor(Math.random() * (results.length)) + 1;
-    console.log(results[rng]);
+    var rng = Math.floor(Math.random() * (results.length)) + 1;
     var i = 0;
     results.sort(function() { return 0.5 - Math.random() });
     var interval = window.setInterval(selectItem, 100);            
     function selectItem() {
-        i + 1 == results.length ? clearInterval(interval) : i++;
+        i + 1 === results.length ? clearInterval(interval) : i++;
         var place = results[i];
         if ( i === rng ) {
             var chosen = results[i];
+            console.log(chosen);
+            
             results.splice(i, 1);
             results.push(chosen);
+            console.log(results);
         }
-
-        createMarker(place, (i === results.length - 1)); 
+        selected = (i === results.length - 1);
+        createMarker(place, selected); 
     } 
 }
 
-function createMarker(place, selected) {
+function createMarker(place) {
     var alpha = (selected) ? 1 : 0.2;
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
@@ -62,14 +74,25 @@ function createMarker(place, selected) {
         opacity: alpha
     });
 
-    var html_name = '<h3 class="place-name">'+place.name+'</h3>';
-    var html_img = '<img src="'+getPlacePhoto(place)+'" class="place-photo"/>';
-
+    var html_content;
+    function buildContent() {
+        var html_name = '<h3 class="place-name">'+place.name+'</h3>';
+        var html_img = '';
+        var html_img = '<img src="'+getPlacePhoto(place)+'" class="place-photo"/>';
+        
+        html_content = html_name;
+        if(html_img.length > 0)
+            html_content = html_content + html_img;
+        
+        return html_content;
+    }
+    console.log(getPlacePhoto(place));
+    
     marker.info = new google.maps.InfoWindow({
-        content: html_name+html_img,
+        content: buildContent(),
         maxWidth: 500
     });
-
+    
     if (selected) {
         var marker_map = marker.getMap();
         marker.info.open(marker_map, marker);
@@ -97,4 +120,5 @@ function getPlacePhoto(place) {
 /*     - Address
 /* randomise again
 /* decouple selection function from data object retrieval
+/* decouple buildcontent from create marker
 */
